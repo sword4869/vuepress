64d20d8ccf1d244e519caaf682a02d79e64b81fc
sword4869 <1084662708@qq.com>
2022-11-29 23:51:02 +0800

    docs

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:sword4869/vuepress.git master:gh-pages
+git push -f git@github.com:sword4869/vuepress-starter.git master:gh-pages
 
 cd -
