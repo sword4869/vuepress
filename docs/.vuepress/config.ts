d152934447d95eda103aa65cd506d148d89a2c7a
sword4869 <1084662708@qq.com>
2022-12-03 19:50:31 +0800

    translator.py

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,34 +4,34 @@ export default defineConfig({
   /**
    * Type is `DefaultThemeConfig`
    */
-  title: 'Hello VuePress',
-  base: '/vuepress-starter/',
+  title: "Hello VuePress",
+  base: "/vuepress-starter/",
   themeConfig: {
     // if your docs are in a different repo from your main project:
     docsRepo: "sword4869/vuepress-starter",
     // if your docs are in a specific branch (defaults to 'master'):
     docsBranch: "main",
     // if your docs are not at the root of the repo:
-    docsDir: 'docs',
+    docsDir: "docs",
     // defaults to false, set to true to enable
     editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: 'Help us improve this page!',
-
+    editLinkText: "Help us improve this page!",
 
     sidebar: {
-      '/': [
+      "/": [
+        ["README.md", "README.md"],
+        ["theme.md", "theme.md"],
         {
-          'title': 'create',
-          'children': [
-            ['/create/空项目.md', '空项目.md'],
-            ['/create/deploy脚本.md', 'deploy脚本.md'],
-            ['/create/github action.md', 'github action.md'],
-            ['/create/图片格式.md', '图片格式.md'],
+          title: "create",
+          children: [
+            ["deploy脚本.md", "deploy脚本.md"],
+            ["github action.md", "github action.md"],
+            ["图片格式.md", "图片格式.md"],
+            ["空项目.md", "空项目.md"],
           ],
         },
-        ['theme.md','theme2.md'],
-      ]
+      ],
     },
-  }
+  },
 });
--- translator.py
+++ translator.py
@@ -1,179 +1,56 @@
 import os
 import sys
 
-path = os.getcwd()
-print(f"path={path}")
-
-
-"""
-for root, dirs, files in a[1:]:
-    print(root)
-    print(dirs)
-    print(files)
-
-    
-    # 用途一：遍历文件
-    for name in files:
-        result.append((root, name))
-
-    # 用途二：遍历所有的文件夹
-    for name in dirs:
-        print(os.path.join(root, name))
-"""
-
-
-walks = [
-    (
-        "",
-        [
-            "命令行",
-            "声音处理",
-            "配置",
-            "图像",
-            "other",
-            "virtual environment",
-            "压缩",
-            "路径",
-            ".vuepress",
-            "控制台效果",
-            "读写文件",
-        ],
-        ["README.md"],
-    ),
-    ("命令行", [], ["readme.md", "sh.py", "sh.ipynb", "tmp_log.log"]),
-    ("声音处理", [], ["pydub.md"]),
-    ("配置", ["配置文件", "读取命令行"], []),
-    ("配置/配置文件", ["csv", "json", "ini", "yaml", "configargparse"], ["regex.md"]),
-    ("配置/配置文件/csv", [], ["csv.ipynb", "some.csv"]),
-    ("配置/配置文件/json", [], ["json.md", "some.json", "json.ipynb"]),
-    ("配置/配置文件/ini", [], ["ini.md"]),
-    ("配置/配置文件/yaml", [], ["yaml.ipynb", "yaml.md", "some.yaml"]),
-    (
-        "配置/配置文件/configargparse",
-        [],
-        ["some.ini", "some.txt", "t.py", "some.yaml", "configargparse.md"],
-    ),
-    ("配置/读取命令行", [], ["readme.md", "argparse.md", "sys and getopt.md"]),
-    ("图像", [], ["PIL.ipynb", "1.jpg", "skvideo.io.ipynb", "ffmpy.md"]),
-    ("other", [], ["adb.md", "the use guidance of jupyter notebook.md", "调试pdb.ipynb"]),
-    (
-        "virtual environment",
-        [],
-        ["pip.md", "requirements.md", "virtualenv.md", "conda.md"],
-    ),
-    ("压缩", [], ["pickle.md"]),
-    ("路径", [], ["readme.md", "t.ipynb", "os.md"]),
-    (".vuepress", [], ["config.ts"]),
-    ("控制台效果", [], ["color.md", "print.md", "进度条.ipynb"]),
-    ("读写文件", [], ["filename.ipynb"]),
-]
-
-
 '''
 [
-      {
+    {
         title: 'create',
         children: [
-          ['/create/空项目.md','空项目.md'],
-          ['/create/deploy脚本.md','deploy脚本.md'],
-          ['/create/github action.md','github action.md'],
-          ['/create/图片格式.md','图片格式.md'],
+            ['/create/空项目.md','空项目.md'],
+            ['/create/deploy脚本.md','deploy脚本.md'],
+            ['/create/github action.md','github action.md'],
+            {
+                title: 'create',
+                children: [
+                    ['/create/空项目.md','空项目.md'],
+                    ['/create/deploy脚本.md','deploy脚本.md'],
+                    ['/create/github action.md','github action.md'],
+                    ['/create/图片格式.md','图片格式.md'],
+                ],
+            },
         ],
-      },
-      {
-        title: 'theme',
-        children: [
-          ['theme.md','theme.md'],
-        ]
-      },
-    ]
-  }
+    },
+    ['theme.md','theme.md']
 ]
 '''
 
 
-items = []
-
-for root, dirs, files in walks:    
-    # 用途一：遍历文件
-    for name in files:
-        print((root, name))
-        items.append((root, name))
 
 
-print(items)
-
-for root, name in items:
-    root_splits = root.split("/")
-    root_splits_length = 0 if root=="" else len(root_splits)
-
-
-
-
-
-
-
-
-def add_directory(part_result, root_splits_length, dirs):
-    for dir in dirs:
-        part_result.append({"title": dir, "children": []})
-
-def add_file(part_result, root_splits_length, files):
+def tree_dir(dir,layer, part_result):
+    files = sorted(os.listdir(dir))
+    file_lst=[]
+    dir_lst=[]
     for file in files:
+        file_path = os.path.join(dir, file)
+        if os.path.isfile(file_path):
+            file_lst.append(file)
+        else:
+            dir_lst.append(file)
+
+    for file in file_lst:
         part_result.append([file, file])
 
+    for subdir in dir_lst:
+        subdir_path = os.path.join(dir, subdir)        
+        part_result.append({"title": subdir, "children": []})
+        tree_dir(subdir_path,layer + 1, part_result[-1]['children'])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-items = list(a)
-k = 1
-def fu(k):
-    print(f'k={k}')
-    j = 0
-    index = 0
-    while index < len(items):
-        root, dirs, files = items[index]
-        
-        if root == "":
-            for dir in dirs:
-                result.append({"title": dir, "children": []})
-            for file in files:
-                result.append([file, file])
-            items.pop(index)
-            break
-
-        root_splits = root.split("/")
-        
-        if len(root_splits) == k:
-            # 直接是文件,就添加
-            if len(dirs) == 0:
-                for file in files:
-                    result[j]["children"].append(file)
-                items.pop(index)
-                j += 1
-                index -=1
-    
-
-        index += 1
-        
-
-    k += 1
-    
-
-
-# while len(items) > 0:
-for k in range(2):
-    fu(k)
-    print(items)
-    print(result)
+result = {
+    'title': 'starter',
+    'children':[]
+}
+tree_dir(sys.argv[1], 0, result['children'])
+print(result)
